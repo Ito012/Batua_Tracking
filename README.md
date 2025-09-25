2bd3011ed45917e82750d3d74f1a60c77b2008aa
Ito012 <113837120+Ito012@users.noreply.github.com>
2025-09-25 23:11:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Batua_Tracking
+Aplikasi tracking pedagang keliling menggunakan Flutter, Google Maps API, dan Firebase
---
+++ android/app/src/main/kotlin/com/example/bakso_tracking/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bakso_tracking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
